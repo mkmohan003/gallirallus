6cf8c994b00af14795ac08b921af6a5f66d89a01
Manoj Mohan <mkmohan003@gmail.com>
2023-07-25 19:28:50 +0530

    add CLIP model

--- cnn_feat_extract_corr.py
+++ cnn_feat_extract_corr.py
@@ -1,3 +1,4 @@
+import clip
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -13,11 +14,18 @@ from torchvision.models.feature_extraction import create_feature_extractor, get_
 from torchvision import transforms
 from tqdm import tqdm
 
-platform = 'local' #'colab'
+PLATFORM = "local"
+BATCH_SIZE = 100
+RAND_SEED = 5
+if PLATFORM == "colab":
+    DATA_DIR = '/content/drive/MyDrive/algonauts_2023_tutorial_data/subj01'
+    DEVICE = "cuda:0"
+    from google.colab import drive
+    drive.mount('/content/drive/', force_remount=True)
+else:
+    DATA_DIR = "subj01"
+    DEVICE = "cpu"
 
-if platform == "colab":
-  from google.colab import drive
-  drive.mount('/content/drive/', force_remount=True)
 
 class ImageDataset(Dataset):
     def __init__(self, imgs_paths, idxs, transform):
@@ -33,169 +41,140 @@ class ImageDataset(Dataset):
         img = Image.open(img_path).convert('RGB')
         # Preprocess the image and send it to the chosen device ('cpu' or 'cuda')
         if self.transform:
-            img = self.transform(img).to(device)
+            img = self.transform(img).to(DEVICE)
         return img
 
-batch_size = 100
-transform = transforms.Compose([
-    transforms.Resize((224,224)), # resize the images to 224x24 pixels
-    transforms.ToTensor(), # convert the images to a PyTorch tensor
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # normalize the images color channels
-])
 
-data_dir = '/content/drive/MyDrive/algonauts_2023_tutorial_data/subj01' if platform == "colab" else "subj01"
-fmri_dir = os.path.join(data_dir, 'training_split', 'training_fmri')
-lh_fmri = np.load(os.path.join(fmri_dir, 'lh_training_fmri.npy'))
-rh_fmri = np.load(os.path.join(fmri_dir, 'rh_training_fmri.npy'))
+def getImageDataLoader(data_dir, transform):
+    train_img_dir = os.path.join(data_dir, 'training_split', 'training_images')
+    test_img_dir = os.path.join(data_dir, 'test_split', 'test_images')
+    train_img_list = os.listdir(train_img_dir)
+    train_img_list.sort()
+    test_img_list = os.listdir(test_img_dir)
+    test_img_list.sort()
+    print('Training images: ' + str(len(train_img_list)))
+    print('Test images: ' + str(len(test_img_list)))
+    # 90 - 10 train-val split. Hardcoded to 8800 to get a multiple of 100
+    num_train = 8800
+    idxs = np.arange(len(train_img_list))
+    np.random.shuffle(idxs)
+    idxs_train, idxs_val = idxs[:num_train], idxs[num_train:]
+    print('Training stimulus images: ' + format(len(idxs_train)))
+    print('\nValidation stimulus images: ' + format(len(idxs_val)))
+    train_imgs_paths = sorted(list(Path(train_img_dir).iterdir()))
+    train_imgs_dataloader = DataLoader(
+        ImageDataset(train_imgs_paths, idxs_train, transform),
+        batch_size=BATCH_SIZE
+    )
+    val_imgs_dataloader = DataLoader(
+        ImageDataset(train_imgs_paths, idxs_val, transform),
+        batch_size=BATCH_SIZE
+    )
 
-print('LH training fMRI data shape:')
-print(lh_fmri.shape)
-print('(Training stimulus images × LH vertices)')
-
-print('\nRH training fMRI data shape:')
-print(rh_fmri.shape)
-print('(Training stimulus images × RH vertices)')
-train_img_dir  = os.path.join(data_dir, 'training_split', 'training_images')
-test_img_dir  = os.path.join(data_dir, 'test_split', 'test_images')
-
-# Create lists will all training and test image file names, sorted
-train_img_list = os.listdir(train_img_dir)
-train_img_list.sort()
-test_img_list = os.listdir(test_img_dir)
-test_img_list.sort()
-print('Training images: ' + str(len(train_img_list)))
-print('Test images: ' + str(len(test_img_list)))
-rand_seed = 5
-np.random.seed(rand_seed)
-
-# Calculate how many stimulus images correspond to 90% of the training data
-# num_train = int(np.round(len(train_img_list) / 100 * 90))
-num_train = 8800
-# Shuffle all training stimulus images
-idxs = np.arange(len(train_img_list))
-np.random.shuffle(idxs)
-# Assign 90% of the shuffled stimulus images to the training partition,
-# and 10% to the test partition
-idxs_train, idxs_val = idxs[:num_train], idxs[num_train:]
-# No need to shuffle or split the test stimulus images
-idxs_test = np.arange(len(test_img_list))
-
-print('Training stimulus images: ' + format(len(idxs_train)))
-print('\nValidation stimulus images: ' + format(len(idxs_val)))
-print('\nTest stimulus images: ' + format(len(idxs_test)))
-
-# Get the paths of all image files
-train_imgs_paths = sorted(list(Path(train_img_dir).iterdir()))
-test_imgs_paths = sorted(list(Path(test_img_dir).iterdir()))
-
-# The DataLoaders contain the ImageDataset class
-train_imgs_dataloader = DataLoader(
-    ImageDataset(train_imgs_paths, idxs_train, transform),
-    batch_size=batch_size
-)
-val_imgs_dataloader = DataLoader(
-    ImageDataset(train_imgs_paths, idxs_val, transform),
-    batch_size=batch_size
-)
-test_imgs_dataloader = DataLoader(
-    ImageDataset(test_imgs_paths, idxs_test, transform),
-    batch_size=batch_size
-)
-lh_fmri_train = lh_fmri[idxs_train]
-lh_fmri_val = lh_fmri[idxs_val]
-rh_fmri_train = rh_fmri[idxs_train]
-rh_fmri_val = rh_fmri[idxs_val]
-# free up RAM
-del lh_fmri, rh_fmri
+    return train_imgs_dataloader, idxs_train, val_imgs_dataloader, idxs_val
 
 
-def fit_pca(feature_extractor, dataloader):
+def getfMRIData(data_dir, idxs_train, idxs_val):
+    fmri_dir = os.path.join(data_dir, 'training_split', 'training_fmri')
+    lh_fmri = np.load(os.path.join(fmri_dir, 'lh_training_fmri.npy'))
+    rh_fmri = np.load(os.path.join(fmri_dir, 'rh_training_fmri.npy'))
+    print(f"LH training fMRI - images * vertices: {lh_fmri.shape}")
+    print(f"RH training fMRI - images * vertices: {rh_fmri.shape}")
+    lh_fmri_train = lh_fmri[idxs_train]
+    lh_fmri_val = lh_fmri[idxs_val]
+    rh_fmri_train = rh_fmri[idxs_train]
+    rh_fmri_val = rh_fmri[idxs_val]
+    # free up RAM
+    del lh_fmri, rh_fmri
+    return lh_fmri_train, rh_fmri_train, lh_fmri_val, rh_fmri_val
+
+
+def fitPCA(feature_extractor, dataloader, flatten):
 
     # Define PCA parameters
-    pca = IncrementalPCA(n_components=100, batch_size=batch_size)
+    pca = IncrementalPCA(n_components=100, batch_size=BATCH_SIZE)
 
     # Fit PCA to batch
     for _, d in tqdm(enumerate(dataloader), total=len(dataloader)):
         # Extract features
         ft = feature_extractor(d)
         # Flatten the features
-        ft = torch.hstack([torch.flatten(l, start_dim=1) for l in ft.values()])
+        if flatten:
+            ft = torch.hstack([torch.flatten(l, start_dim=1)
+                               for l in ft.values()])
         # Fit PCA to batch
         pca.partial_fit(ft.detach().cpu().numpy())
     return pca
 
 
-def extract_features(feature_extractor, dataloader, pca):
+def extractFeatures(feature_extractor, dataloader, pca, flatten):
 
     features = []
     for _, d in tqdm(enumerate(dataloader), total=len(dataloader)):
         # Extract features
         ft = feature_extractor(d)
         # Flatten the features
-        ft = torch.hstack([torch.flatten(l, start_dim=1) for l in ft.values()])
+        if flatten:
+            ft = torch.hstack([torch.flatten(l, start_dim=1)
+                               for l in ft.values()])
         # Apply PCA transform
         ft = pca.transform(ft.cpu().detach().numpy())
         features.append(ft)
     return np.vstack(features)
 
 
-def calculate_correlation(model_name, device):
+def getModel(model_name):
     model = None
+    feature_extractor = None
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
     if model_name == "alexnet":
         model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet')
-        model.to(device)
-        model.eval()
         model_layer = "features.2"
+        feature_extractor = create_feature_extractor(model,
+                                                     return_nodes=[model_layer])
     elif model_name == "efficientnet_b0":
-        model = efficientnet_b0(weights = EfficientNet_B0_Weights.IMAGENET1K_V1)
-        model.to(device)
-        model.eval()
+        model = efficientnet_b0(weights=EfficientNet_B0_Weights.IMAGENET1K_V1)
         model_layer = 'features.3.1.add'
-    else:
-        print("Unsupported model")
-        import sys
-        sys.exit()
+        feature_extractor = create_feature_extractor(model,
+                                                     return_nodes=[model_layer])
+    elif model_name == "clip":
+        model, transform = clip.load("ViT-B/32")
+        feature_extractor = model.encode_image
 
-    feature_extractor = create_feature_extractor(model, return_nodes=[model_layer])
-    pca = fit_pca(feature_extractor, train_imgs_dataloader)
-    features_train = extract_features(feature_extractor, train_imgs_dataloader, pca)
-    features_val = extract_features(feature_extractor, val_imgs_dataloader, pca)
-    features_test = extract_features(feature_extractor, test_imgs_dataloader, pca)
+    return model, feature_extractor, transform
 
-    print('\nTraining images features:')
-    print(features_train.shape)
-    print('(Training stimulus images × PCA features)')
 
-    print('\nValidation images features:')
-    print(features_val.shape)
-    print('(Validation stimulus images × PCA features)')
-
-    print('\nTest images features:')
-    print(features_val.shape)
-    print('(Test stimulus images × PCA features)')
-
-# Fit linear regressions on the training data
+def fitEncodingModel(features_train, lh_fmri_train, rh_fmri_train):
     reg_lh = LinearRegression().fit(features_train, lh_fmri_train)
     reg_rh = LinearRegression().fit(features_train, rh_fmri_train)
-# Use fitted linear regressions to predict the validation and test fMRI data
+    return reg_lh, reg_rh
+
+
+def predictWithEncodingModel(reg_lh, reg_rh, features_val):
     lh_fmri_val_pred = reg_lh.predict(features_val)
-    lh_fmri_test_pred = reg_lh.predict(features_test)
     rh_fmri_val_pred = reg_rh.predict(features_val)
-    rh_fmri_test_pred = reg_rh.predict(features_test)
+    return lh_fmri_val_pred, rh_fmri_val_pred
 
-# Empty correlation array of shape: (LH vertices)
+
+def calculateCorrelation(lh_fmri_val, rh_fmri_val, lh_fmri_val_pred,
+                         rh_fmri_val_pred):
     lh_correlation = np.zeros(lh_fmri_val_pred.shape[1])
-# Correlate each predicted LH vertex with the corresponding ground truth vertex
     for v in tqdm(range(lh_fmri_val_pred.shape[1])):
-        lh_correlation[v] = corr(lh_fmri_val_pred[:,v], lh_fmri_val[:,v])[0]
+        lh_correlation[v] = corr(lh_fmri_val_pred[:, v], lh_fmri_val[:, v])[0]
 
-# Empty correlation array of shape: (RH vertices)
     rh_correlation = np.zeros(rh_fmri_val_pred.shape[1])
-# Correlate each predicted RH vertex with the corresponding ground truth vertex
     for v in tqdm(range(rh_fmri_val_pred.shape[1])):
-        rh_correlation[v] = corr(rh_fmri_val_pred[:,v], rh_fmri_val[:,v])[0]
+        rh_correlation[v] = corr(rh_fmri_val_pred[:, v], rh_fmri_val[:, v])[0]
 
-# Load the ROI classes mapping dictionaries
+    return lh_correlation, rh_correlation
+
+
+def plotCorrelationMap(lh_correlation, rh_correlation, data_dir):
+    # Load the ROI classes mapping dictionaries
     roi_mapping_files = ['mapping_prf-visualrois.npy', 'mapping_floc-bodies.npy',
         'mapping_floc-faces.npy', 'mapping_floc-places.npy',
         'mapping_floc-words.npy', 'mapping_streams.npy']
@@ -204,7 +183,7 @@ def calculate_correlation(model_name, device):
         roi_name_maps.append(np.load(os.path.join(data_dir, 'roi_masks', r),
             allow_pickle=True).item())
 
-# Load the ROI brain surface maps
+    # Load the ROI brain surface maps
     lh_challenge_roi_files = ['lh.prf-visualrois_challenge_space.npy',
         'lh.floc-bodies_challenge_space.npy', 'lh.floc-faces_challenge_space.npy',
         'lh.floc-places_challenge_space.npy', 'lh.floc-words_challenge_space.npy',
@@ -221,7 +200,7 @@ def calculate_correlation(model_name, device):
         rh_challenge_rois.append(np.load(os.path.join(data_dir, 'roi_masks',
             rh_challenge_roi_files[r])))
 
-# Select the correlation results vertices of each ROI
+    # Select the correlation results vertices of each ROI
     roi_names = []
     lh_roi_correlation = []
     rh_roi_correlation = []
@@ -233,23 +212,61 @@ def calculate_correlation(model_name, device):
                 rh_roi_idx = np.where(rh_challenge_rois[r1] == r2[0])[0]
                 lh_roi_correlation.append(lh_correlation[lh_roi_idx])
                 rh_roi_correlation.append(rh_correlation[rh_roi_idx])
+
+    print(f"Overall: L:{lh_correlation.mean()},R:{rh_correlation.mean()}")
     roi_names.append('All vertices')
     lh_roi_correlation.append(lh_correlation)
     rh_roi_correlation.append(rh_correlation)
 
-# Create the plot
     lh_mean_roi_correlation = [np.mean(lh_roi_correlation[r])
         for r in range(len(lh_roi_correlation))]
     rh_mean_roi_correlation = [np.mean(rh_roi_correlation[r])
         for r in range(len(rh_roi_correlation))]
 
-    print(lh_correlation.mean(), rh_correlation.mean())
+    plt.figure(figsize=(18, 6))
+    x = np.arange(len(roi_names))
+    width = 0.30
+    plt.bar(x - width/2, lh_mean_roi_correlation, width, label='Left Hemisphere')
+    plt.bar(x + width/2, rh_mean_roi_correlation, width, label='Right Hemishpere')
+    plt.xlim(left=min(x)-.5, right=max(x)+.5)
+    plt.ylim(bottom=0, top=1)
+    plt.xlabel('ROIs')
+    plt.xticks(ticks=x, labels=roi_names, rotation=60)
+    plt.ylabel('Mean Pearson\'s $r$')
+    plt.legend(frameon=True, loc=1)
+    plt.show()
+
     lh_mean_roi_correlation = np.array(lh_mean_roi_correlation)
-    print(lh_mean_roi_correlation[~np.isnan(lh_mean_roi_correlation)].mean())
+    print(f"ROI: L {lh_mean_roi_correlation[~np.isnan(np.array(lh_mean_roi_correlation))].mean()}")
     rh_mean_roi_correlation = np.array(rh_mean_roi_correlation)
-    print(rh_mean_roi_correlation[~np.isnan(rh_mean_roi_correlation)].mean())
+    print(f"ROI: R {rh_mean_roi_correlation[~np.isnan(rh_mean_roi_correlation)].mean()}")
 
 
-model_name = "efficientnet_b0"
-device = "cpu" #"cuda:0"
-calculate_correlation(model_name, device)
+if __name__ == "__main__":
+    np.random.seed(RAND_SEED)
+    model_name = "clip"
+    flatten = False if model_name == "clip" else True
+    model, feature_extractor, transform = getModel(model_name)
+    if model is not None:
+        model.eval()
+        model.to(DEVICE)
+        train_imgs_loader, ids_train, val_imgs_loader, ids_val =  \
+                getImageDataLoader(DATA_DIR, transform)
+        lh_fmri_train, rh_fmri_train, lh_fmri_val, rh_fmri_val = \
+                getfMRIData(DATA_DIR, ids_train, ids_val)
+        pca = fitPCA(feature_extractor, train_imgs_loader, flatten)
+        features_train = extractFeatures(feature_extractor, train_imgs_loader,
+                                         pca, flatten)
+        features_val = extractFeatures(feature_extractor, val_imgs_loader,
+                                       pca, flatten)
+        print(f"training image features: {features_train.shape}")
+        print(f"validation image features: {features_val.shape}")
+        reg_lh, reg_rh = fitEncodingModel(features_train, lh_fmri_train,
+                                          rh_fmri_train)
+        lh_fmri_val_pred, rh_fmri_val_pred = \
+                predictWithEncodingModel(reg_lh, reg_rh, features_val)
+        lh_correlation, rh_correlation = calculateCorrelation(lh_fmri_val,
+                                                              rh_fmri_val,
+                                                              lh_fmri_val_pred,
+                                                              rh_fmri_val_pred)
+        plotCorrelationMap(lh_correlation, rh_correlation, DATA_DIR)
